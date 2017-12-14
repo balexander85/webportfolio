f72af0a6f65c15cc7250706a9d1c43b73f8869d1
Brian Alexander <brian@dadgumsalsa.com>
2017-12-14 08:20:08 -0800

    Was missing stuff for static files in the urls.py

--- public/webportfolio/webportfolio/urls.py
+++ public/webportfolio/webportfolio/urls.py
@@ -15,10 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
 
 from .views import index
 
 urlpatterns = [
     path('', index, name="index"),
     path('admin/', admin.site.urls),
+    url(
+        r'^static/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.STATIC_ROOT}
+    ),
 ]
+
